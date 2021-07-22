e688f80ad44dc6c4d2edfec3b1a8a3772b5ae1f3
BriWS <brightwiths@gmail.com>
2021-07-22 19:52:40 +0300

    lesson complete

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import s from './Main.module.css';
+import s from './Main.module.scss';
+import photo from '../assets/images/myPhoto.jpg'
+import sContainer from '../common/styles/Container.module.css'
 
 export function Main() {
   return (
     <div className={s.mainBlock}>
+      <div className={sContainer.container}>
+        <div className={s.text}>
+          <span>Hi There</span>
+          <h1>I am Sergey Ustyugov</h1>
+          <p>Frontend Developer.</p>
+        </div>
+        <div className={s.photo}></div>
+      </div>
 
     </div>
   );
---
+++ src/skills/Skills.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './Skills.module.scss';
+import sContainer from '../common/styles/Container.module.css'
+import {Skill} from "./skill/Skill";
+
+export function Skills() {
+  return (
+    <div className={s.skillsBlock}>
+      <div className={`${sContainer.container} ${s.skillsContainer}`}>
+        <h2 className={s.title}>Skills</h2>
+        <div className={s.skills}>
+          <Skill title={'React'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, obcaecati?'}/>
+          <Skill title={'Redux'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolorem, dolores fuga nesciunt ratione voluptates.'}/>
+          <Skill title={'JS'} description={'Lorem ipsum dolor sit amet.'}/>
+          {/*<Skill title={'HTML'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo.'}/>
+          <Skill title={'CSS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, inventore tempora.'}/>*/}
+        </div>
+      </div>
+    </div>
+  );
+}
+
