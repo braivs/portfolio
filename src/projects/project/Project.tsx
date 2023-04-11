b16f631b3787750baee825aaf69c0fb4f7dd620e
Briws <brightwiths@gmail.com>
2023-04-11 17:16:00 +0530

    Fixed cursor pointer on project

--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import s from './Project.module.scss';
+import React from 'react'
+import s from './Project.module.scss'
 
 type ProjectPropsType = {
-  title: string
-  description: string
-  link: string
-  style: React.CSSProperties
+    title: string
+    description: string
+    link: string
+    style: React.CSSProperties
 }
 
 export function Project(props: ProjectPropsType) {
-  return (
-    <div className={s.project}>
-      <div className={s.image} style={props.style} onClick={() => window.open(props.link, "_blank")}>
-        <div className={s.viewBtn}>Watch</div>
-      </div>
-      <div className={s.projectInfo}>
-        <h3 className={s.projectTitle}>{props.title}</h3><br/>
-        <div className={s.description}>{props.description}</div>
-      </div>
-    </div>
-  );
+    return (
+        <div className={s.project}>
+            <div className={s.image} style={props.style} onClick={() => window.open(props.link, "_blank")}>
+                <div className={s.viewBtn}>Watch</div>
+            </div>
+            <div className={s.projectInfo}>
+                <h3 className={s.projectTitle}>{props.title}</h3><br/>
+                <div className={s.description}>{props.description}</div>
+            </div>
+        </div>
+    )
 }
 
