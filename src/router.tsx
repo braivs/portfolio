28760b8299da1769dd50a1abf831a668e7026e12
Briws <brightwiths@gmail.com>
2023-10-04 11:38:01 +0400

    last

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import {createBrowserRouter, RouteObject, RouterProvider} from "react-router-dom
 import {Portfolio} from "./components/Portfolio/Portfolio"
 import {CV} from "./components/CV/CV"
 
-const publicRoutes: RouteObject[] = [
+/*const publicRoutes: RouteObject[] = [
   {
     path: '/',
     element: <Portfolio />,
@@ -11,10 +11,17 @@ const publicRoutes: RouteObject[] = [
     path: '/cv',
     element: <CV />,
   },
-]
+]*/
 
 const router = createBrowserRouter([
-  ...publicRoutes,
+  {
+    path: '/',
+    element: <Portfolio />,
+  },
+  {
+    path: '/cv',
+    element: <CV />,
+  },
 ])
 
 export const Router = () => {
