0a34282a5b1bd3b34e891fbf7f316ae10281befe
Briws <brightwiths@gmail.com>
2024-09-18 01:18:40 +0700

    returned mobile BurgerNav

--- src/components/Portfolio/header/burgerNav/BurgerNav.tsx
+++ src/components/Portfolio/header/burgerNav/BurgerNav.tsx
@@ -15,13 +15,13 @@ export function BurgerNav() {
     return (
         <div className={s.burgerNav}>
             <div className={menuIsOpen ? `${s.burgerNavItems} ${s.show}` : s.burgerNavItems}>
-                {/*<Link activeClass={s.active} to="main" spy={true} smooth={true} offset={-80} duration={500}>Main</Link>
+                <Link activeClass={s.active} to="main" spy={true} smooth={true} offset={-80} duration={500}>Main</Link>
                 <Link activeClass={s.active} to="skills" spy={true} smooth={true} offset={-80}
                       duration={500}>Skills</Link>
                 <Link activeClass={s.active} to="projects" spy={true} smooth={true} offset={-80}
                       duration={500}>Projects</Link>
                 <Link activeClass={s.active} to="contacts" spy={true} smooth={true} offset={-80}
-                      duration={500}>Contacts</Link>*/}
+                      duration={500}>Contacts</Link>
             </div>
             {
                 menuIsOpen
